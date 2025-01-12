e060a02b032c08b124dfa4a4fd5010e17e202aa5
spyguel <pabloperezdisanti@gmail.com>
2025-01-11 21:58:16 -0300

    implementación inicial de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Login from './pages/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Login></Login>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
